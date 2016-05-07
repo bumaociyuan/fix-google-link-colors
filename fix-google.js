582cbc2647b7446a6db44b99d13473c61581c323
Ben R <ben.rabidou@gmail.com>
2016-05-06 20:02:26 -0400

    Tampermonkey script to fix it

---
+++ fix-google.js
@@ -0,0 +1,25 @@
+// ==UserScript==
+// @name         Fix google colors
+// @namespace    http://twitter.com/brabidou
+// @version      0.1
+// @description  Fixes google link colors back to normal
+// @author       You
+// @match        https://www.google.com/search*
+// @match        http://www.google.com/search*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    function addGlobalStyle(css) {
+        var head, style;
+        head = document.getElementsByTagName('head')[0];
+        if (!head) { return; }
+        style = document.createElement('style');
+        style.type = 'text/css';
+        style.innerHTML = css;
+        head.appendChild(style);
+    }
+    addGlobalStyle('a:link { color: #1a0dab; }');
+    addGlobalStyle('a:visited { color: #609; }');
+})();
